866dc45c44dac1e605fe42e7e678e95e43f26f46
Andy Kemp <40425562+andrew-kemp@users.noreply.github.com>
2025-10-15 19:19:53 +0100

    SSO

--- public_html/admin_login.php
+++ public_html/admin_login.php
@@ -4,7 +4,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 session_start();
-require_once(__DIR__ . '/config/config.php');
+require_once(__DIR__ . '/../config/config.php');
 
 function esc($x) { return htmlspecialchars($x ?? '', ENT_QUOTES); }
 
